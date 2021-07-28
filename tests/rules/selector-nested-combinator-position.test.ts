1c1be68f025e0becd8d94221c523f8b3b265d9a2
CyanSalt <c.salt@hotmail.com>
2021-07-28 14:53:21 +0800

    refactor: unify source code directories

--- tests/rules/selector-nested-combinator-position.test.ts
+++ tests/rules/selector-nested-combinator-position.test.ts
@@ -1,11 +1,11 @@
 import * as path from 'path';
 import getTestRule from 'jest-preset-stylelint/getTestRule';
-import { messages, ruleName } from '../rules/selector-nested-combinator-position';
+import { messages, ruleName } from '../../src/rules/selector-nested-combinator-position';
 
 const testRule = getTestRule();
 
 testRule({
-  plugins: [path.resolve(__dirname, '../rules/selector-nested-combinator-position.ts')],
+  plugins: [path.resolve(__dirname, '../../src/rules/selector-nested-combinator-position.ts')],
   ruleName,
   config: ['as-prefix'],
   fix: true,
