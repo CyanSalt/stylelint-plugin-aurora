0bec3afc6b1c5461b28b5cf7f9a1804c19cbddc4
CyanSalt <c.salt@hotmail.com>
2025-11-28 14:58:16 +0800

    fix: type issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import type { Plugin } from 'stylelint'
 import selectorNestedCombinatorPosition from './rules/selector-nested-combinator-position.js'
 
 export default [
   selectorNestedCombinatorPosition,
-]
+] satisfies Plugin[]
